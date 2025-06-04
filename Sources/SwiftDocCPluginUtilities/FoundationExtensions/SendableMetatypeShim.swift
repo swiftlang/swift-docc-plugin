405b3398710c6e6bb1e46f5032f2bc4a6b5085d5
David Rönnqvist <ronnqvist@apple.com>
2025-06-04 16:09:51 +0200

    Update the necessary license header comment style

--- Sources/SwiftDocCPluginUtilities/FoundationExtensions/SendableMetatypeShim.swift
+++ Sources/SwiftDocCPluginUtilities/FoundationExtensions/SendableMetatypeShim.swift
@@ -1,12 +1,10 @@
-/*
- This source file is part of the Swift.org open source project
-
- Copyright (c) 2025 Apple Inc. and the Swift project authors
- Licensed under Apache License v2.0 with Runtime Library Exception
-
- See https://swift.org/LICENSE.txt for license information
- See https://swift.org/CONTRIBUTORS.txt for Swift project authors
-*/
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 
 // In Swift 6.2, metatypes are no longer sendable by default (SE-0470).
 // Instead a type needs to conform to `SendableMetatype` to indicate that its metatype is sendable.
